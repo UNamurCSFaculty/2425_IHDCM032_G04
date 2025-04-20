3128b3586a0a41568c1c5b0350276d32f8435f24
fabriceci <fabricecipolla@gmail.com>
2025-04-20 14:14:32 +0200

    setup project

---
+++ client/src/store/userStore.tsx
@@ -0,0 +1,14 @@
+import { create } from 'zustand'
+import type { User } from '@/types/api'
+
+interface UserState {
+  user: User | null
+  setUser: (user: User) => void
+  logout: () => void
+}
+
+export const useUserStore = create<UserState>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  logout: () => set({ user: null }),
+}))
