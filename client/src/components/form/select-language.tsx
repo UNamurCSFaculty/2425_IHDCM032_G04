a9cc511bd39da5311784e2a112608f8353583775
fabriceci <fabricecipolla@gmail.com>
2025-04-27 22:25:22 +0200

    Add select langage field

---
+++ client/src/components/form/select-language.tsx
@@ -0,0 +1,52 @@
+import { useFieldContext } from '.'
+import { Label } from '../ui/label'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select'
+import { FieldErrors } from './field-errors'
+
+type Language = {
+  id: number
+  langue: string
+}
+
+const languages: Language[] = [
+  { id: 1, langue: 'fr' },
+  { id: 2, langue: 'en' },
+]
+
+export function SelectLanguageField() {
+  const field = useFieldContext<Language>()
+  console.log(field.state.value)
+  const currentId = field.state.value.id.toString()
+
+  return (
+    <div className="space-y-2">
+      <Label htmlFor={field.name}>Langue</Label>
+      <Select
+        value={currentId}
+        onValueChange={val => {
+          const id = Number(val)
+          const langObj = languages.find(l => l.id === id)!
+          field.handleChange(langObj)
+        }}
+      >
+        <SelectTrigger id={field.name} onBlur={field.handleBlur}>
+          <SelectValue placeholder="Choisissez une langue" />
+        </SelectTrigger>
+        <SelectContent>
+          {languages.map(l => (
+            <SelectItem key={l.id} value={l.id.toString()}>
+              {l.langue}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <FieldErrors meta={field.state.meta} />
+    </div>
+  )
+}
