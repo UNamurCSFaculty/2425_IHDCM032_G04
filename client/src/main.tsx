81924980d90deb5d8fbfa986b2a146033c8b7434
fabriceci <fabricecipolla@gmail.com>
2025-05-02 23:47:05 +0200

    Use tanstack query on auth

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,17 @@ import { StrictMode, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Observer } from 'tailwindcss-intersect'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 // initialisation i18n
 import './i18n'
 import '@/utils/zod-config.ts'
 
-// Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
 import './styles.css'
@@ -21,15 +24,14 @@ import { useUserStore } from './store/userStore.tsx'
 import { GlobalSkeleton } from './components/GlobalSkeleton.tsx'
 
 import { client } from '@/api/generated/client.gen.ts'
-import { getCurrentUser } from './api/generated/sdk.gen.ts'
+import { getCurrentUserOptions } from './api/generated/@tanstack/react-query.gen.ts'
+
 client.setConfig({
-  credentials: 'include',
+  credentials: 'include', // pour les cookies (HTTP Only)
 })
 
-// Create React Query client
 const queryClient = new QueryClient()
 
-// Create a new router instance
 const router = createRouter({
   routeTree,
   context: {
@@ -44,65 +46,43 @@ const router = createRouter({
   defaultPendingComponent: GlobalSkeleton,
 })
 
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
-
-// Render the app
 function AppWithProvider() {
-  // Appel du Hook à l’intérieur d’un composant React
-  const user = useUserStore(state => state.user)
-  const setUser = useUserStore(state => state.setUser)
-  const loading = useUserStore(state => state.loading)
-  const setLoading = useUserStore(state => state.setLoading)
+  const user = useUserStore(s => s.user)
+  const setUser = useUserStore(s => s.setUser)
+
+  const { data, isLoading } = useQuery({
+    ...getCurrentUserOptions(),
+    retry: false, // pas de retry pour current user
+    staleTime: Infinity, // jamais stale tant qu’on est connecté
+  })
 
   useEffect(() => {
-    async function fetchUser() {
-      const { data, error } = await getCurrentUser()
-      if (!error && data) {
-        setUser(data)
-      }
-
-      setLoading(false)
+    if (data) {
+      setUser(data)
     }
+  }, [data, setUser])
 
-    fetchUser()
-  }, [setLoading, setUser])
-
-  if (loading) {
+  if (isLoading) {
     return <GlobalSkeleton />
   }
 
   return (
     <ErrorBoundary>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} context={{ user }} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      <RouterProvider router={router} context={{ user }} />
     </ErrorBoundary>
   )
 }
 
-const rootElement = document.getElementById('app')
-if (rootElement && !rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('app')!)
+root.render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
       <AppWithProvider />
-    </StrictMode>
-  )
-}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
+)
 
-// Require for
 Observer.start()
-router.subscribe('onRendered', () => {
-  Observer.restart()
-})
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+router.subscribe('onRendered', () => Observer.restart())
 reportWebVitals()
