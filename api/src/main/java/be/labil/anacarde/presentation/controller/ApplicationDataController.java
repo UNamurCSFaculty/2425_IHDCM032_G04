c0ab34055680d46a82e98420d6678f3e384189dd
fabriceci <fabricecipolla@gmail.com>
2025-05-04 11:49:02 +0200

    typo

--- api/src/main/java/be/labil/anacarde/presentation/controller/ApplicationDataController.java
+++ api/src/main/java/be/labil/anacarde/presentation/controller/ApplicationDataController.java
@@ -1,5 +1,6 @@
 package be.labil.anacarde.presentation.controller;
 
+import be.labil.anacarde.application.service.ApplicationDataService;
 import be.labil.anacarde.domain.dto.ApplicationDataDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ApplicationDataController implements ApplicationData {
 
+	private final ApplicationDataService applicationDataService;
+
 	@Override
 	public ApplicationDataDto getApplicationData() {
-		return null;
+		return applicationDataService.getApplicationData();
 	}
 }
