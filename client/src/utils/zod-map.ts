3beebcc18071d312ead60b0b0791bfbe3a7beb59
SteXpe <xperience.s.glibert@gmail.com>
2025-05-05 11:58:10 +0200

    Spécification et correction des messages d'erreurs.

--- client/src/utils/zod-map.ts
+++ client/src/utils/zod-map.ts
@@ -1,11 +1,12 @@
-import { type ZodErrorMap } from 'zod'
 import i18n from '../i18n'
+import { type ZodErrorMap } from 'zod'
 
 export const customErrorMap: ZodErrorMap = issue => {
   console.log(issue, issue.message, issue.code)
   switch (issue.code) {
     case 'too_small': {
       const min = issue.minimum as number
+      if (min === 1) return { message: i18n.t('validation.required') }
       return { message: i18n.t('validation.minLength', { count: min }) }
     }
     case 'too_big': {
@@ -14,7 +15,12 @@ export const customErrorMap: ZodErrorMap = issue => {
     }
     case 'invalid_format': {
       // issue.validation est "email" | "url" | …
-      return { message: i18n.t(`validation.${issue.validation}`) }
+      if (issue.format === 'regex')
+        return { message: i18n.t(`validation.${issue.path}`) }
+      return { message: i18n.t(`validation.${issue.format}`) }
+    }
+    case 'custom': {
+      return { message: i18n.t(`validation.${issue.path}`) }
     }
     default:
       return { message: 'Error Occurred' }
