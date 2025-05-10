777eb400af7c4cbbc74e145e84fca9e9e6d82e71
Fabrice Cipolla <fabricecipolla@gmail.com>
2025-05-10 21:56:07 +0200

    fix few typo and format (#54)

--- client/src/components/auctions/BidsDialog.tsx
+++ client/src/components/auctions/BidsDialog.tsx
@@ -1,37 +1,56 @@
-import React from 'react';
-import * as Dialog from '@radix-ui/react-dialog';
-import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from '@/components/ui/table';
+import { BidForm } from './BidForm'
+import {
+  acceptAuctionMutation,
+  acceptBidMutation,
+  listBidsOptions,
+} from '@/api/generated/@tanstack/react-query.gen'
 import { Button } from '@/components/ui/button'
-import {useQuery } from '@tanstack/react-query'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { formatDate } from '@/lib/utils'
+import * as Dialog from '@radix-ui/react-dialog'
+import { useQuery } from '@tanstack/react-query'
 import { useMutation } from '@tanstack/react-query'
-import { acceptAuctionMutation, acceptBidMutation, listBidsOptions } from '@/api/generated/@tanstack/react-query.gen'
-import { formatDate } from '@/lib/utils';
-import { BidForm } from './BidForm';
+import React from 'react'
 
 interface BidsDialogProps {
-  auctionId: number;
-  isOpen: boolean;
-  openChange: (open: boolean) => void;
-  showColumnAcceptBid?: boolean;
-  showBidForm?: boolean;
+  auctionId: number
+  isOpen: boolean
+  openChange: (open: boolean) => void
+  showColumnAcceptBid?: boolean
+  showBidForm?: boolean
 }
 
-const BidsDialog: React.FC<BidsDialogProps> = ({ auctionId, isOpen, openChange, showColumnAcceptBid, showBidForm }) => {
-  const { data: bidsData, isLoading, isError } = useQuery(
-    {
-      ...listBidsOptions({ path: { auctionId: auctionId } }),
-      enabled: !!auctionId,
-    }
-  );
+const BidsDialog: React.FC<BidsDialogProps> = ({
+  auctionId,
+  isOpen,
+  openChange,
+  showColumnAcceptBid,
+  showBidForm,
+}) => {
+  const {
+    data: bidsData,
+    isLoading,
+    isError,
+  } = useQuery({
+    ...listBidsOptions({ path: { auctionId: auctionId } }),
+    enabled: !!auctionId,
+  })
 
-  const { mutate: acceptAuction } = useMutation(acceptAuctionMutation());
-  const { mutate: acceptBid } = useMutation(acceptBidMutation());
+  const { mutate: acceptAuction } = useMutation(acceptAuctionMutation())
+  const { mutate: acceptBid } = useMutation(acceptBidMutation())
 
   const handleAcceptBid = (bidId: number) => {
-    acceptBid({ path: { auctionId, bidId }});
-    acceptAuction({ path: { id: auctionId }});
-    openChange(false);
-  };
+    acceptBid({ path: { auctionId, bidId } })
+    acceptAuction({ path: { id: auctionId } })
+    openChange(false)
+  }
 
   return (
     <Dialog.Root open={isOpen} onOpenChange={openChange}>
@@ -41,57 +60,67 @@ const BidsDialog: React.FC<BidsDialogProps> = ({ auctionId, isOpen, openChange, 
           aria-describedby={undefined}
           className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded-lg shadow-lg w-auto max-w-[90%]"
         >
-          <Dialog.Title className="text-xl font-bold">Offres sur l'enchère</Dialog.Title>
+          <Dialog.Title className="text-xl font-bold">
+            Offres sur l'enchère
+          </Dialog.Title>
           <Dialog.Close asChild>
-            <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700">✕</button>
+            <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700">
+              ✕
+            </button>
           </Dialog.Close>
           <div className="mt-4">
             {isLoading && <p>Chargement des offres...</p>}
-            {isError && <p>Une erreur s'est produite lors du chargement des offres.</p>}
-            {(!bidsData || bidsData.length == 0)
-              ? (
-                  <p>Aucune offre trouvée pour cette enchère.</p>
-                ) 
-              : (
-                  <Table>
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead>Date</TableHead>
-                      <TableHead>Enchérisseur</TableHead>
-                      <TableHead>Montant</TableHead>
-                      { showColumnAcceptBid && <TableHead></TableHead> }
+            {isError && (
+              <p>Une erreur s'est produite lors du chargement des offres.</p>
+            )}
+            {!bidsData || bidsData.length == 0 ? (
+              <p>Aucune offre trouvée pour cette enchère.</p>
+            ) : (
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Date</TableHead>
+                    <TableHead>Enchérisseur</TableHead>
+                    <TableHead>Montant</TableHead>
+                    {showColumnAcceptBid && <TableHead></TableHead>}
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {bidsData.map(bid => (
+                    <TableRow key={bid.id}>
+                      <TableCell>{formatDate(bid.creationDate)}</TableCell>
+                      <TableCell>
+                        {bid.trader.firstName} {bid.trader.lastName}
+                      </TableCell>
+                      <TableCell>{bid.amount.toLocaleString()} CFA</TableCell>
+                      {showColumnAcceptBid && (
+                        <TableCell>
+                          <Button
+                            onClick={() => {
+                              handleAcceptBid(bid.id)
+                            }}
+                          >
+                            Accepter
+                          </Button>
+                        </TableCell>
+                      )}
                     </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    {bidsData.map((bid) => (
-                        <TableRow key={bid.id}>
-                          <TableCell>{formatDate(bid.creationDate)}</TableCell>
-                          <TableCell>
-                            {bid.trader.firstName} {bid.trader.lastName}
-                          </TableCell>
-                          <TableCell>{bid.amount.toLocaleString()} CFA</TableCell>
-                          {
-                            showColumnAcceptBid &&
-                            <TableCell>
-                              <Button onClick={() => { handleAcceptBid(bid.id); }}>Accepter</Button>
-                            </TableCell>
-                          }
-                        </TableRow>
-                      ))
-                    }
-                  </TableBody>
-                </Table>
-              )
-            }
-            </div>
+                  ))}
+                </TableBody>
+              </Table>
+            )}
+          </div>
 
-            {
-              showBidForm && <BidForm auctionId={auctionId} onMakeBid={() => openChange(false) } />
-            }
+          {showBidForm && (
+            <BidForm
+              auctionId={auctionId}
+              onMakeBid={() => openChange(false)}
+            />
+          )}
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
-  );
-};
+  )
+}
 
-export default BidsDialog;
+export default BidsDialog
--- client/src/routes/_authenticated/achats/historique.tsx
+++ client/src/routes/_authenticated/achats/historique.tsx
@@ -1,33 +1,35 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { useQuery } from '@tanstack/react-query'
-import { listAuctionsOptions } from '@/api/generated/@tanstack/react-query.gen'
 import { type AuctionDto } from '@/api/generated'
-import { useUserStore } from '@/store/userStore'
+import { listAuctionsOptions } from '@/api/generated/@tanstack/react-query.gen'
 import AuctionsTable from '@/components/auctions/AuctionsTable'
+import { useUserStore } from '@/store/userStore'
+import { useQuery } from '@tanstack/react-query'
+import { createFileRoute } from '@tanstack/react-router'
 
 const listAuctionsQueryOptions = (userId: number) => ({
-  ...listAuctionsOptions({ query: { buyerId: userId, status: "Accepté" } }),
+  ...listAuctionsOptions({ query: { buyerId: userId, status: 'Accepté' } }),
   staleTime: 10_000,
-});
+})
 
 export const Route = createFileRoute('/_authenticated/achats/historique')({
   component: RouteComponent,
   loader: async ({ context: { queryClient, user } }) => {
-    return queryClient.ensureQueryData(listAuctionsQueryOptions(user!.id));
+    return queryClient.ensureQueryData(listAuctionsQueryOptions(user!.id))
   },
-});
+})
 
 export function RouteComponent() {
-  const { user } = useUserStore();
+  const { user } = useUserStore()
 
-  const { data } = useQuery(listAuctionsQueryOptions(user!.id));
+  const { data } = useQuery(listAuctionsQueryOptions(user!.id))
 
-  const historyAuctions = data as AuctionDto[];
+  const historyAuctions = data as AuctionDto[]
 
-  return <AuctionsTable 
-            tableTitle="Mes achats passés" 
-            showColumnBidder={true}
-            showColumnBidderPrice={true}
-            auctions={historyAuctions} 
-          />
+  return (
+    <AuctionsTable
+      tableTitle="Mes achats passés"
+      showColumnBidder={true}
+      showColumnBidderPrice={true}
+      auctions={historyAuctions}
+    />
+  )
 }
