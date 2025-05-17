088d05c119362de92793fe0a513f7eaeec26172f
fabriceci <fabricecipolla@gmail.com>
2025-05-17 17:07:57 +0200

    Fix suite

--- api/src/main/java/be/labil/anacarde/presentation/controller/UserApi.java
+++ api/src/main/java/be/labil/anacarde/presentation/controller/UserApi.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.StringToClassMapItem;
 import io.swagger.v3.oas.annotations.media.*;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
@@ -40,48 +39,29 @@ public interface UserApi {
 	 * Récupère les informations détaillées d'un utilisateur à partir de son identifiant.
 	 *
 	 * @param id
-	 *            L'identifiant de l'utilisateur.
-	 * @return Une ResponseEntity contenant les détails de l'utilisateur.
+	 *            L'identifiant de l'utilisateur à récupérer.
+	 * @return Les détails de l'utilisateur.
 	 */
 	@Operation(summary = "Récupérer les détails d'un utilisateur", description = "Renvoie les informations détaillées d'un utilisateur à partir de son ID.")
 	@ApiResponses({
 			@ApiResponse(responseCode = "200", description = "Utilisateur trouvé", content = @Content(schema = @Schema(implementation = UserDetailDto.class))),
-			@ApiResponse(responseCode = "404", description = "Utilisateur non trouvé", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),})
+			@ApiResponse(responseCode = "404", description = "Utilisateur non trouvé", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))})
 	@GetMapping("/{id}")
 	ResponseEntity<? extends UserDetailDto> getUser(
 			@NotNull(message = "L'ID de l'utilisateur est obligatoire") @Positive(message = "L'ID doit être un entier positif") @Parameter(description = "Identifiant de l'utilisateur", example = "1", required = true) @PathVariable("id") Integer id);
 
 	/**
-	 * Crée un nouvel utilisateur en utilisant le groupe de validation "Create", qui rend le champ
-	 * mot de passe obligatoire.
-	 *
+	 * Crée un nouvel utilisateur (et, éventuellement, téléverse des documents associés).
+	 * 
 	 * @param user
-	 *            Les détails de l'utilisateur à créer.
+	 *            Détails de l'utilisateur à créer.
 	 * @param documents
-	 *            Liste de fichiers à télécharger pour l'utilisateur.
-	 * @return Une ResponseEntity contenant les détails de l'utilisateur créé.
+	 *            Documents à téléverser
+	 * @return L'utilisateur créé.
 	 */
-	/*
-	 * @Operation(summary = "Créer un utilisateur", description =
-	 * "Crée un nouvel utilisateur dans le système.") // @Parameter(name = "type", description =
-	 * "Type d'utilisateur. Valeurs possibles: ", example = // "admin", required = // true)
-	 * 
-	 * @ApiResponses({
-	 * 
-	 * @ApiResponse(responseCode = "201", description = "Utilisateur créé avec succès", content
-	 * = @Content(schema = @Schema(implementation = UserDetailDto.class, discriminatorProperty =
-	 * "type"))),
-	 * 
-	 * @ApiResponse(responseCode = "400", description = "Erreur de validation ou JSON invalide",
-	 * content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),
-	 * 
-	 * @ApiResponse(responseCode = "409", description = "Conflit avec un utilisateur existant",
-	 * content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))})
-	 * 
-	 * @PostMapping(consumes = "application/json")
-	 */
-	@Operation(summary = "Créer un utilisateur et téléverser éventuellement des documents")
-	@RequestBody(required = true, content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE, schema = @Schema(type = "object", properties = {
+	@Operation(summary = "Créer un utilisateur et téléverser des documents")
+	@io.swagger.v3.oas.annotations.parameters.RequestBody
+	(required = true, content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE, schema = @Schema(type = "object", properties = {
 			@StringToClassMapItem(key = "user", value = UserUpdateDto.class),
 			@StringToClassMapItem(key = "documents", value = MultipartFile[].class)}), encoding = {
 					@Encoding(name = "user", contentType = MediaType.APPLICATION_JSON_VALUE),
@@ -97,9 +77,9 @@ public interface UserApi {
 			@RequestPart(value = "documents", required = false) List<MultipartFile> documents);
 
 	/**
-	 * Récupère la liste de tous les utilisateurs.
-	 *
-	 * @return Une ResponseEntity contenant la liste de tous les utilisateurs.
+	 * Renvoie la liste de tous les utilisateurs.
+	 * 
+	 * @return La liste des utilisateurs.
 	 */
 	@Operation(summary = "Lister tous les utilisateurs", description = "Renvoie la liste de tous les utilisateurs présents dans le système.")
 	@ApiResponses({
@@ -108,36 +88,35 @@ public interface UserApi {
 	ResponseEntity<List<? extends UserListDto>> listUsers();
 
 	/**
-	 * Met à jour un utilisateur existant en utilisant le groupe de validation "Update", qui rend le
-	 * champ mot de passe optionnel.
-	 *
+	 * Met à jour un utilisateur existant.
+	 * 
 	 * @param id
 	 *            L'identifiant de l'utilisateur à mettre à jour.
-	 * @param userDetailDto
-	 *            Les nouvelles données de l'utilisateur.
-	 * @return Une ResponseEntity contenant les détails de l'utilisateur mis à jour.
+	 * @param userUpdateDto
+	 *            Détails de l'utilisateur à mettre à jour.
+	 * @return L'utilisateur mis à jour.
 	 */
-	@Operation(summary = "Mettre à jour un utilisateur", description = "Met à jour un utilisateur existant en utilisant l'ID spécifié dans l'URL.")
+	@Operation(summary = "Mettre à jour un utilisateur", description = "Met à jour un utilisateur existant en utilisant l'ID spécifié.")
 	@ApiResponses({
 			@ApiResponse(responseCode = "200", description = "Utilisateur mis à jour avec succès", content = @Content(schema = @Schema(implementation = UserDetailDto.class))),
 			@ApiResponse(responseCode = "400", description = "Erreur de validation ou JSON invalide", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),
-			@ApiResponse(responseCode = "404", description = "Utilisateur non trouvé", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),})
-	@PutMapping(value = "/{id}", consumes = "application/json")
+			@ApiResponse(responseCode = "404", description = "Utilisateur non trouvé", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))})
+	@PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<? extends UserDetailDto> updateUser(
 			@NotNull(message = "L'ID de l'utilisateur est obligatoire") @Positive(message = "L'ID doit être un entier positif") @Parameter(description = "Identifiant de l'utilisateur", example = "1", required = true) @PathVariable("id") Integer id,
 			@Validated({Default.class,
-					ValidationGroups.Update.class}) @RequestBody UserUpdateDto userDetailDto);
+					ValidationGroups.Update.class}) @RequestBody UserUpdateDto userUpdateDto);
 
 	/**
-	 * Supprime un utilisateur à partir de son identifiant.
-	 *
+	 * Supprime un utilisateur.
+	 * 
 	 * @param id
 	 *            L'identifiant de l'utilisateur à supprimer.
 	 */
 	@Operation(summary = "Supprimer un utilisateur", description = "Supprime un utilisateur en fonction de son identifiant.")
 	@ApiResponses({
 			@ApiResponse(responseCode = "204", description = "Utilisateur supprimé avec succès"),
-			@ApiResponse(responseCode = "404", description = "Utilisateur non trouvé", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),})
+			@ApiResponse(responseCode = "404", description = "Utilisateur non trouvé", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))})
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	ResponseEntity<Void> deleteUser(
@@ -145,38 +124,38 @@ public interface UserApi {
 
 	/**
 	 * Ajoute un rôle à un utilisateur.
-	 *
+	 * 
 	 * @param id
 	 *            L'identifiant de l'utilisateur.
 	 * @param roleName
 	 *            Le nom du rôle à ajouter.
-	 * @return Une ResponseEntity contenant l'utilisateur mis à jour.
+	 * @return L'utilisateur misà jour.
 	 */
-	@Operation(summary = "Ajouter un rôle à un utilisateur", description = "Ajoute un rôle spécifique à l'utilisateur en utilisant le nom du rôle.")
+	@Operation(summary = "Ajouter un rôle à un utilisateur", description = "Ajoute un rôle spécifique à l'utilisateur.")
 	@ApiResponses({
 			@ApiResponse(responseCode = "200", description = "Rôle ajouté avec succès", content = @Content(schema = @Schema(implementation = UserDetailDto.class))),
 			@ApiResponse(responseCode = "400", description = "Erreur de validation ou JSON invalide", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),
-			@ApiResponse(responseCode = "404", description = "Utilisateur ou rôle non trouvé", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),})
-	@PostMapping(value = "/{id}/roles/{roleName}")
+			@ApiResponse(responseCode = "404", description = "Utilisateur ou rôle non trouvé", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))})
+	@PostMapping("/{id}/roles/{roleName}")
 	ResponseEntity<? extends UserDetailDto> addRoleToUser(
 			@NotNull(message = "L'ID de l'utilisateur est obligatoire") @Positive(message = "L'ID doit être un entier positif") @Parameter(description = "Identifiant de l'utilisateur", example = "1", required = true) @PathVariable("id") Integer id,
 			@NotNull(message = "Le nom du rôle est obligatoire") @Parameter(description = "Nom du rôle", example = "ROLE_USER", required = true) @PathVariable("roleName") String roleName);
 
 	/**
-	 * Met à jour l'ensemble des rôles d'un utilisateur.
-	 *
+	 * Remplace complètement les rôles d'un utilisateur.
+	 * 
 	 * @param id
 	 *            L'identifiant de l'utilisateur.
 	 * @param roleNames
-	 *            La liste des noms de rôle à attribuer à l'utilisateur.
-	 * @return Une ResponseEntity contenant l'utilisateur mis à jour.
+	 *            Liste des noms de rôles à attribuer.
+	 * @return L'utilisateur mis à jour.
 	 */
-	@Operation(summary = "Mettre à jour les rôles d'un utilisateur", description = "Remplace l'ensemble des rôles de l'utilisateur spécifié par la liste fournie.")
+	@Operation(summary = "Mettre à jour les rôles d'un utilisateur", description = "Remplace l'ensemble des rôles de l'utilisateur spécifié.")
 	@ApiResponses({
 			@ApiResponse(responseCode = "200", description = "Rôles mis à jour avec succès", content = @Content(schema = @Schema(implementation = UserDetailDto.class))),
 			@ApiResponse(responseCode = "400", description = "Erreur de validation ou JSON invalide", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),
-			@ApiResponse(responseCode = "404", description = "Utilisateur ou rôle non trouvé", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class))),})
-	@PutMapping(value = "/{id}/roles", consumes = "application/json")
+			@ApiResponse(responseCode = "404", description = "Utilisateur ou rôle non trouvé", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))})
+	@PutMapping(value = "/{id}/roles", consumes = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<? extends UserDetailDto> updateUserRoles(
 			@NotNull(message = "L'ID de l'utilisateur est obligatoire") @Positive(message = "L'ID doit être un entier positif") @Parameter(description = "Identifiant de l'utilisateur", example = "1", required = true) @PathVariable("id") Integer id,
 			@NotNull(message = "La liste des rôles est obligatoire") @RequestBody List<String> roleNames);
