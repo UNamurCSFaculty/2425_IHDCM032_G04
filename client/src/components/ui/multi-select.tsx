4ca6956c000b1944df4ccf8b149eb1fb3f4b1fb9
zakensen <zakensen@foo.mail>
2025-05-25 12:33:12 +0200

    fix as child

--- client/src/components/ui/multi-select.tsx
+++ client/src/components/ui/multi-select.tsx
@@ -193,7 +193,7 @@ export const MultiSelect = React.forwardRef<
         onOpenChange={setIsPopoverOpen}
         modal={modalPopover}
       >
-        <PopoverTrigger asChild>
+        <PopoverTrigger asChild={asChild}>
           <Button
             ref={ref}
             {...props}
